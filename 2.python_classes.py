53a50054b829f58cee36f52cbfaf07249b199872
Pi <paingthetko.pi@gmail.com>
2020-05-22 22:25:17 +0800

    Create 2.python_classes.py

---
+++ 2.python_classes.py
@@ -0,0 +1,9 @@
+class animal:
+number_of_legs = 0
+dog = animal()
+dog.number_of_legs = 4
+print ("Dog has {} legs".format(dog.number_of_legs))
+'''
+you can also write like that
+print("Dog has " + str(dog.number_of_legs) + " legs")
+'''
